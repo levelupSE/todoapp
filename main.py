82d9baf3c47ba8705c015f761a127c491124b9b0
Work <workmoderately@gmail.com>
2022-07-24 09:51:18 -0700

    add home page message

--- main.py
+++ main.py
@@ -1,15 +1,34 @@
 import psycopg2
 
-if __name__ == '__main__':
-    db = psycopg2.connect(**{
-        'dbname': 'todo_app',
-        'password': 'password',
-        'user': 'postgres',
-        'port': '54320',
-        'host': 'localhost'
-    })
+db = psycopg2.connect(**{
+    'dbname': 'todo_app',
+    'password': 'password',
+    'user': 'postgres',
+    'port': '54320',
+    'host': 'localhost'
+})
+
+def get_todos():
     with db.cursor() as cur:
         cur.execute('SELECT * FROM todo;')
         results = cur.fetchall()
+    return results
 
-    print(f'Results: {results}')
+def insert_todo(description):
+    with db.cursor() as cur:
+        cur.execute('INSERT INTO todo (description) VALUES (%s)', (description,))
+
+def get_homepage_message():
+    todos = get_todos()
+    if not todos:
+        return 'All your TODOs are done!'
+
+    todo_descriptions = ', '.join([todo[1] for todo in todos])
+    return f'Here are your todos: {todo_descriptions}'
+
+if __name__ == '__main__':
+    print(get_homepage_message())
+    insert_todo('something')
+    print(get_homepage_message())
+    insert_todo('something1')
+    print(get_homepage_message())
--- scripts/init.sql
+++ scripts/init.sql
@@ -3,7 +3,7 @@ CREATE DATABASE todo_app;
 \c todo_app;
 
 CREATE TABLE IF NOT EXISTS todo (
-  todo_id UUID PRIMARY KEY,
+  todo_id SERIAL PRIMARY KEY,
   description TEXT NOT NULL
 );
 
